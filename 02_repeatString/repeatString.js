98bc26b4ac65141e29f3405130e0cafeadd2dce3
Kelvin <121232509+kelvinyunan@users.noreply.github.com>
2024-06-15 04:17:15 +0700

    progress 15/06/2024

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word,count) {
+    let a = ""
+    let i = 0;
 
+    if (count > 0) {
+    while (i < count) {
+        a += word
+        i++
+    }} else if (count == 0){
+        return ""
+    }else {
+        return 'ERROR'
+    }
+
+    return a; 
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    const arrWord = words.split('').reverse().join('');
+    
+    return arrWord
 };
 
 // Do not edit below this line
