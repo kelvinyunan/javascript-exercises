2bc6cede67539d2d71ecf732ec06cc4af1d9dae2
Kelvin <121232509+kelvinyunan@users.noreply.github.com>
2024-06-15 18:43:55 +0700

    progress 2 15/06/2024

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    let count = 0;
 
+    if (typeof start == 'number' && typeof end == 'number') {
+        if (start > 0 && end > 0) {
+            if (end > start) {
+                for (let i = start; i <= end; i++) {
+                    count += i
+                }
+            } else {
+                for (let i = end; i <= start; i++) {
+                    count += i
+                }
+            }
+        } else return 'ERROR'
+    } else return 'ERROR'
+    
+    return count;
 };
 
 // Do not edit below this line
